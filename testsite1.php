86e400eedd689b4941666a981abacd1fc6b0def0
Riz <rizwanulrudra@gmail.com>
2015-11-13 13:25:18 +0700

    AJAX Call (First try)
    Just follow the codes inside /* 
                                                       */ 
    Let me know if it works or not!

--- testsite1.php
+++ testsite1.php
@@ -25,6 +25,34 @@
 	<title>Midterm Ja</title>
 </head>
 <body>
+	/*
+	
+<script>
+function showUser(str) {
+  if (str=="") {
+    document.getElementById("id2").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else { // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("id2").innerHTML=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("GET","testsite2.php?q="+str,true);
+  xmlhttp.send();
+}
+
+</script>
+	
+	
+	*/
+	
 	<h1>Midterm Examinationnnn</h1>
 
         
